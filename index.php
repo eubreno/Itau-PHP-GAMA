a8275c75bdf9d46f6ebe10cd7019d9dca62c5a0f
eubreno <eubreno@gmail.com>
2021-01-14 15:00:27 -0300

    Quarto commit do arquivo php

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
+<!DOCTYPE HTML>
 <html>
- <head>
-  <title>Teste PHP</title>
- </head>
- <body>
- <?php echo "<p>Olá Mundo</p>"; ?>
- </body>
+<head><title>Teste</title>
+<meta charset='UTF-8'>
+<style type='text/css'>
+body{background:#000000;color:#ffffff;font-family:courier;margin:0px;width:98%;padding:1%;}
+h1{color:#8c7853;font-size:120%;text-align:left;clear:both;}
+h2{color:#cfb53d;}
+img{float:right;width:20%;height:15%;margin:5px;border:1px solid #999999;}
+p{color:#ffffff;font-size:100%;text-align:left;}
+a:link, a:visited{color:#ffffbb;}
+marquee{width:98%;padding:1%;color:#ffffff;background:#000000;font-size:100%;}
+</style>
+</head>
+<body>
+<h1>Teste</h1>
+<img src="https://www.novidades100.com.br/gadget/web/images/003.jpg" alt="Teste" title="Teste">
+<h2>Teste</h2>
+<a href="https://https://breno-first-php.herokuapp.com/" alt="Teste" title="Teste">https://breno-first-php.herokuapp.com/</a>
+<p>Teste</p>
+<marquee>Teste</marquee>
+</body>
 </html>
