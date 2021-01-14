3d2cfe828938ec65773b97bfb6fa9853376d66f7
eubreno <eubreno@gmail.com>
2021-01-14 14:45:43 -0300

    Terceiro commit do arquivo php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<dl>
-  <dt>solilóquio</dt>
-  <dd>No drama, onde um personagem fala a si mesmo, representando seus pensamentos ou sentimentos internos e no processo, transmitindo-os ao público (mas não a outros personagens).</dd>
-  <dt>monólogo</dt>
-  <dd>No drama, onde um personagem fala seus pensamentos em voz alta para compartilhá-los com o público e com outros personagens presentes.</dd>
-  <dt>aparte</dt>
-  <dd>No drama, onde um personagem compartilha um comentário apenas com o público para efeito humorístico ou dramático. Isso geralmente é um sentimento, pensamento ou parte de informação de fundo adicional.</dd>
-</dl>
+<html>
+ <head>
+  <title>Teste PHP</title>
+ </head>
+ <body>
+ <?php echo "<p>Olá Mundo</p>"; ?>
+ </body>
+</html>
